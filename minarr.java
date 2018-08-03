c95a2fee2f7c990351291b052c6aad3176454d61
ramyachowdary4 <41936857+ramyachowdary4@users.noreply.github.com>
2018-08-03 14:32:29 +0530

    Create minarr.java

---
+++ minarr.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class minarr
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int min=a[0];
+		for(int i=0;i<n;i++)
+		{
+			if(min>a[i])
+			{
+				min=a[i];
+			}
+		}
+		System.out.println(min);
+	}
+}
